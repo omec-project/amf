941528a582fd228e1a3e302c28ec0970b9b13bf5
Vini Gajjar <vini.gajjar@gslab.com>
2021-07-28 08:18:15 +0530

    SDCORE-301: AMF Unit Test for NGAP setup code

---
+++ ngap/ngap_test.go
@@ -0,0 +1,81 @@
+// SPDX-FileCopyrightText: 2021 Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+
+package ngap_test
+
+import (
+	"log"
+	"testing"
+
+	ngaputil "github.com/free5gc/amf/ngap/util"
+
+	"github.com/free5gc/amf/context"
+	"github.com/free5gc/amf/factory"
+	"github.com/free5gc/amf/ngap"
+	"github.com/free5gc/amf/util"
+)
+
+func init() {
+	// Initializing AMF Context from config.
+	testAmfConfig := "../amfTest/amfcfg.yaml"
+	if err := factory.InitConfigFactory(testAmfConfig); err != nil {
+		log.Fatal("Failed to initialzie Factory Config")
+	}
+
+	util.InitAmfContext(context.AMF_Self())
+}
+
+// TestHandleNGSetupRequest validates package ngap's handling for NGSetupRequest
+func TestHandleNGSetupRequest(t *testing.T) {
+	// test cases
+	testTable := []struct {
+		gnbName, tac string
+		gnbId        []byte
+		bitLength    uint64
+		want, testId byte
+	}{
+		// expecting SuccessfulOutcome
+		{
+			testId:    1,
+			gnbName:   "GNB2",
+			tac:       "\x00\x00\x01",
+			gnbId:     []byte{0x00, 0x00, 0x08},
+			bitLength: 22,
+			want:      ngaputil.NgapPDUSuccessfulOutcome,
+		},
+		// expecting UnsuccessfulOutcome due to unsupported TA
+		{
+			testId:    2,
+			gnbName:   "GNB2",
+			tac:       "\x00\x00\x04",
+			gnbId:     []byte{0x00, 0x00, 0x08},
+			bitLength: 22,
+			want:      ngaputil.NgapPDUUnSuccessfulOutcome,
+		},
+	}
+
+	conn := &ngaputil.TestConn{}
+	for _, test := range testTable {
+		testNGSetupReq, err := ngaputil.GetNGSetupRequest(test.gnbId, test.bitLength, test.gnbName, test.tac)
+
+		if err != nil {
+			t.Log("Failed to to create NGSetupRequest")
+			return
+		}
+		ngap.Dispatch(conn, testNGSetupReq)
+
+		// conn.data holds the NGAP response message
+		if len(conn.Data) == 0 {
+			t.Error("Unexpected message drop")
+			return
+		}
+
+		// The first byte of the NGAPPDU indicates the type of NGAP Message
+		if conn.Data[0] != test.want {
+			t.Error("Test case", test.testId, "failed.  Want:",
+				ngaputil.MessageTypeMap[test.want], ",  Got:", ngaputil.MessageTypeMap[conn.Data[0]])
+		}
+	}
+}
