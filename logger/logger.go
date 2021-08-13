4a50150d3dfafb84562813802e420572d53f5e1a
Pingping Lin <pingping@opennetworking.org>
2021-08-13 10:32:36 -0500

    do not create amf.log & do not append log to free5gc.log

--- logger/logger.go
+++ logger/logger.go
@@ -1,14 +1,14 @@
 package logger
 
 import (
-	"os"
+//	"os"
 	"time"
 
 	formatter "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/sirupsen/logrus"
 
-	"github.com/free5gc/logger_conf"
-	"github.com/free5gc/logger_util"
+//	"github.com/free5gc/logger_conf"
+//	"github.com/free5gc/logger_util"
 )
 
 var (
@@ -51,16 +51,16 @@ func init() {
 		FieldsOrder:     []string{"component", "category", FieldRanAddr, FieldAmfUeNgapID, FieldSupi},
 	}
 
-	free5gcLogHook, err := logger_util.NewFileHook(logger_conf.Free5gcLogFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o666)
+/*	free5gcLogHook, err := logger_util.NewFileHook(logger_conf.Free5gcLogFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o666)
 	if err == nil {
 		log.Hooks.Add(free5gcLogHook)
 	}
 
-	selfLogHook, err := logger_util.NewFileHook(logger_conf.NfLogDir+"amf.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o666)
+	selfLogHook, err := logger_util.NewFileHook(logger_conf.NfLogDir+"amf-pingping.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o666)
 	if err == nil {
 		log.Hooks.Add(selfLogHook)
 	}
-
+*/
 	AppLog = log.WithFields(logrus.Fields{"component": "AMF", "category": "App"})
 	InitLog = log.WithFields(logrus.Fields{"component": "AMF", "category": "Init"})
 	CfgLog = log.WithFields(logrus.Fields{"component": "AMF", "category": "CFG"})
