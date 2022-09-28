b87cc7706ff23ff2e6fe4d5cc6357620a146c3df
Ajay Lotan Thakur <thakur.ajay@gmail.com>
2022-09-28 04:55:12 -0700

    Fix readbuffer size in AMF. (#85)
    Keeping it just 8k causes lot of message drop at the socket level.

--- ngap/service/service.go
+++ ngap/service/service.go
@@ -24,7 +24,7 @@ type NGAPHandler struct {
 	HandleNotification func(conn net.Conn, notification sctp.Notification)
 }
 
-const readBufSize uint32 = 8192
+const readBufSize uint32 = 131072
 
 // set default read timeout to 2 seconds
 var readTimeout syscall.Timeval = syscall.Timeval{Sec: 2, Usec: 0}
