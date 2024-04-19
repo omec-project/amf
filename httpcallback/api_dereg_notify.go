6664805c748b0f47ee20309c0d50266a3dfa55b0
akhila-s06 <94685258+akhila-s06@users.noreply.github.com>
2024-04-19 09:15:58 +0530

    paging fix,handle deregistration notification implimentation (#214)
    Co-authored-by: gab-arrobo <gabriel.arrobo@intel.com>

---
+++ httpcallback/api_dereg_notify.go
@@ -0,0 +1,68 @@
+// SPDX-FileCopyrightText: 2022 Infosys Limited
+// Copyright 2019 free5GC.org
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package httpcallback
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/omec-project/amf/logger"
+	"github.com/omec-project/amf/producer"
+	"github.com/omec-project/openapi"
+	"github.com/omec-project/openapi/models"
+	"github.com/omec-project/util/httpwrapper"
+)
+
+func HTTPDeregistrationNotification(c *gin.Context) {
+	var deregistrationData models.DeregistrationData
+
+	requestBody, err := c.GetRawData()
+	if err != nil {
+		logger.CallbackLog.Errorf("Get Request Body error: %+v", err)
+		problemDetail := models.ProblemDetails{
+			Title:  "System failure",
+			Status: http.StatusInternalServerError,
+			Detail: err.Error(),
+			Cause:  "SYSTEM_FAILURE",
+		}
+		c.JSON(http.StatusInternalServerError, problemDetail)
+		return
+	}
+
+	err = openapi.Deserialize(&deregistrationData, requestBody, "application/json")
+	if err != nil {
+		problemDetail := "[Request Body] " + err.Error()
+		rsp := models.ProblemDetails{
+			Title:  "Malformed request syntax",
+			Status: http.StatusBadRequest,
+			Detail: problemDetail,
+		}
+		logger.CallbackLog.Errorln(problemDetail)
+		c.JSON(http.StatusBadRequest, rsp)
+		return
+	}
+
+	req := httpwrapper.NewRequest(c.Request, deregistrationData)
+	if supi, exists := c.Params.Get("supi"); exists {
+		req.Params["supi"] = supi
+	}
+	rsp := producer.HandleDeregistrationNotification(req)
+
+	responseBody, err := openapi.Serialize(rsp.Body, "application/json")
+	if err != nil {
+		logger.CallbackLog.Errorln(err)
+		problemDetails := models.ProblemDetails{
+			Status: http.StatusInternalServerError,
+			Cause:  "SYSTEM_FAILURE",
+			Detail: err.Error(),
+		}
+		c.JSON(http.StatusInternalServerError, problemDetails)
+	} else {
+		c.Data(rsp.Status, "application/json", responseBody)
+	}
+
+}
