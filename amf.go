a21be8e3c74c01ac030f5213b924e980a3f1ea8c
Arrobo, Gabriel <gabriel.arrobo@intel.com>
2025-06-18 20:47:12 -0700

    Migrate code to use cli/v3
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- amf.go
+++ amf.go
@@ -8,30 +8,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/omec-project/amf/logger"
 	"github.com/omec-project/amf/service"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v3"
 )
 
 var AMF = &service.AMF{}
 
 func main() {
-	app := cli.NewApp()
+	app := &cli.Command{}
 	app.Name = "amf"
 	logger.AppLog.Infoln(app.Name)
 	app.Usage = "Access & Mobility Management function"
 	app.UsageText = "amf -cfg <amf_config_file.conf>"
 	app.Action = action
 	app.Flags = AMF.GetCliCmd()
-	if err := app.Run(os.Args); err != nil {
+	if err := app.Run(context.Background(), os.Args); err != nil {
 		logger.AppLog.Fatalf("AMF run error: %v", err)
 	}
 }
 
-func action(c *cli.Context) error {
+func action(ctx context.Context, c *cli.Command) error {
 	if err := AMF.Initialize(c); err != nil {
 		logger.CfgLog.Errorf("%+v", err)
 		return fmt.Errorf("failed to initialize")
