6ac04b8d5713a6a3f2bd1231360502438cfb8c3d
gab-arrobo <gabriel.arrobo@intel.com>
2025-09-03 08:51:20 -0700

    Make minor improvements and create patch release (#512)
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- context/transaction.go
+++ context/transaction.go
@@ -11,13 +11,13 @@ import (
 )
 
 type EventChannel struct {
-	Message       chan interface{}
+	Message       chan any
 	Event         chan string
 	AmfUe         *AmfUe
 	NasHandler    func(*AmfUe, NasMsg)
 	NgapHandler   func(*AmfUe, NgapMsg)
-	SbiHandler    func(ctx context.Context, s1, s2 string, msg interface{}) (interface{}, string, interface{}, interface{})
-	ConfigHandler func(ctx context.Context, s1, s2, s3 string, msg interface{})
+	SbiHandler    func(ctx context.Context, s1, s2 string, msg any) (any, string, any, any)
+	ConfigHandler func(ctx context.Context, s1, s2, s3 string, msg any)
 }
 
 func (tx *EventChannel) UpdateNgapHandler(handler func(*AmfUe, NgapMsg)) {
@@ -30,12 +30,12 @@ func (tx *EventChannel) UpdateNasHandler(handler func(*AmfUe, NasMsg)) {
 	tx.NasHandler = handler
 }
 
-func (tx *EventChannel) UpdateSbiHandler(handler func(ctx context.Context, s1, s2 string, msg interface{}) (interface{}, string, interface{}, interface{})) {
+func (tx *EventChannel) UpdateSbiHandler(handler func(ctx context.Context, s1, s2 string, msg any) (any, string, any, any)) {
 	tx.AmfUe.TxLog.Infof("updated sbihandler")
 	tx.SbiHandler = handler
 }
 
-func (tx *EventChannel) UpdateConfigHandler(handler func(ctx context.Context, s1, s2, s3 string, msg interface{})) {
+func (tx *EventChannel) UpdateConfigHandler(handler func(ctx context.Context, s1, s2, s3 string, msg any)) {
 	tx.AmfUe.TxLog.Infof("updated confighandler")
 	tx.ConfigHandler = handler
 }
@@ -70,6 +70,6 @@ func (tx *EventChannel) Start(ctx context.Context) {
 	}
 }
 
-func (tx *EventChannel) SubmitMessage(msg interface{}) {
+func (tx *EventChannel) SubmitMessage(msg any) {
 	tx.Message <- msg
 }
